fb85b250c7e1519760ac82dab44d9af2b9bf0142
nafelaraz <nafel.araz@gmail.com>
2015-12-03 00:25:13 +0200

    duzenleme

--- odev03/caesar_thread.py
+++ odev03/caesar_thread.py
@@ -1,14 +1,16 @@
 
 
 
+
 alfabe = 'abcdefghijklmnopqrstuvwxyz'
 anahtar={}
+s=input("kac harf saga kaydirilacagini giriniz :")
 
 j=0
 
 while j<26:
 
-    anahtar[alfabe[j]]=alfabe[j-3]
+    anahtar[alfabe[j]]=alfabe[j-s]
     j+=1
 
 print('alfabe=' , alfabe)
