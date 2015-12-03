407324ac99401c36f4c0f905b95586a1ac903f45
nafelaraz <nafel.araz@gmail.com>
2015-12-03 02:38:12 +0200

    duzenleme

--- odev03/caesar_thread.py
+++ odev03/caesar_thread.py
@@ -10,6 +10,7 @@ l=input("blok uzunlugunu giriniz :")
 isim="crypted_"+str(s)+"_"+ str(n)+"_"+str(l)+".txt"
 print(isim)
 dosya = open(isim,"w")
+dosya.close()
 
 j=0
 
@@ -20,5 +21,17 @@ while j<26:
 
 print('alfabe=' , alfabe)
 print('anahtar=' , anahtar)
+blok="nafel"
+sifreli=[]
+def sifrele(blok):
+    i=0
+    while i<l:
+        sifreli.append(anahtar[blok[i]])
+        i+=1
+    print(sifreli)
 
-#def sifrele():
+sifrele(blok)
+
+#oku=open("metin.txt","r")
+#dosya=open(isim,"a")
+#dosya.write(oku.read(blok))
