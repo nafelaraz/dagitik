b3a5462bf55669dd10d3dc1fa41e94447da51e10
nafelaraz <nafel.araz@gmail.com>
2015-12-03 19:44:34 +0200

    odev4

--- odev04/message_server.py
+++ odev04/message_server.py
@@ -12,23 +12,13 @@ class myThread (threading.Thread):
         print "Starting " + self.name
         # Get lock to synchronize threads
         threadLock.acquire()
-        s = socket.socket() # Create a socket object
-        host = socket.gethostname() # Get local machine name
-        port = 1234 # Reserve a port for your service.
-        s.bind((host, port)) # Bind to the port
+        #s = socket.socket() # Create a socket object
+        #host = socket.gethostname() # Get local machine name
+        #port = 12344 # Reserve a port for your service.
+        #s.bind((host, port)) # Bind to the port
+        c.send('Thank you for connecting!')
 
-        s.listen(5) # Now wait for client connection.
 
-        while True:
-            global threadID
-            c, addr = s.accept() # Establish connection with client.
-            print 'Got connection from', addr
-            c.send('Thank you for connecting!')
-            tName="thread"+str(threadID)
-            thread = myThread(threadID, tName, 1)
-            thread.start()
-            threads.append(thread)
-            threadID+=1
 
         # Free lock to release next thread
         threadLock.release()
@@ -37,11 +27,23 @@ class myThread (threading.Thread):
 threadLock = threading.Lock()
 threads = []
 threadID = 1
-tName="Main"
-thread = myThread(threadID, tName, 1)
-thread.start()
-threads.append(thread)
-threadID+=1
+s = socket.socket() # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 1234 # Reserve a port for your service.
+s.bind((host, port)) # Bind to the port
+
+s.listen(5) # Now wait for client connection.
+
+while True:
+
+    c, addr = s.accept() # Establish connection with client.
+    print 'Got connection from', addr
+
+    tName="thread"+str(threadID)
+    thread = myThread(threadID, tName, c)
+    thread.start()
+    threads.append(thread)
+    threadID+=1
 
 for t in threads:
     t.join()
